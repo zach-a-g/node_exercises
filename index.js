4b52f3e30c1f0dd5df2fb014e634ea7ef440d27e
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-06-15 15:22:08 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1) Run node in the terminal and console.log() the string "Hello World."
+
+// Answer:
+// console.log("Hello World.");
+
+// 2) Create an index.js file, and have it console.log() the string "Hello World, I am in Node."
+// Run node and execute the file.
+
+// Answer:
+console.log("Hello World. I am in Node."); // run in terminal
+
+// 3) Print out the version of npm that you have installed
+
+// Answer: (in terminal)
+// $ npm --version
+//    6.14.13
